48cb5900a77498e5ee8150de5069900296a5af19
Dawid Ciężarkiewicz <dpc@ucore.info>
2015-02-21 19:55:56 -0800

    Fix Grues getting stuck.

--- src/ai.rs
+++ src/ai.rs
@@ -4,7 +4,7 @@ use std::sync::{Arc,mpsc};
 
 use hex2dext::algo::bfs;
 
-use hex2d::{self, Coordinate};
+use hex2d::{self, Coordinate, ToCoordinate};
 use hex2d as h2d;
 use game;
 use actor;
@@ -19,7 +19,6 @@ fn roam() -> game::Action {
     }
 }
 
-
 fn closest_reachable<F>(gstate : &game::State, start : Coordinate, max_distance : i32, cond : F) -> Option<(Coordinate, Coordinate)>
     where F : Fn(Coordinate) -> bool
 {
@@ -32,32 +31,43 @@ fn closest_reachable<F>(gstate : &game::State, start : Coordinate, max_distance 
     bfs.find().map(|pos| (pos, bfs.backtrace_last(pos).unwrap()))
 }
 
-
 fn grue(astate : &actor::State, gstate : &game::State) -> game::Action {
 
     for &visible_pos in &astate.visible {
         if gstate.actor_map_or(visible_pos, false, |a| a.is_player()) {
-            return go_to(astate, visible_pos);
+            return go_to(visible_pos, astate, gstate);
         }
     }
 
     roam()
 }
 
-fn go_to(astate : &actor::State, c: Coordinate) -> game::Action {
+fn go_to(c: Coordinate, astate : &actor::State, gstate : &game::State) -> game::Action {
     let ndir = astate.pos.coord.direction_to_cw(c).expect("bfs gave me trash");
 
-    if ndir == astate.pos.dir {
-        return game::Action::Move(hex2d::Angle::Forward)
+    let n_pos = astate.pos + ndir.to_coordinate();
+    if gstate.tile_map_or(n_pos.coord, false, |t| t.type_.is_passable()) {
+        if ndir == astate.pos.dir {
+            return game::Action::Move(hex2d::Angle::Forward)
+        } else {
+            return game::Action::Turn(ndir - astate.pos.dir)
+        }
+    }
+    //TODO: fallaback to A* instead of BFS
+    let reachable = closest_reachable(gstate, astate.pos.coord, 10,
+                                   |pos| pos == c
+                                  );
+
+    if let Some((_, n)) = reachable {
+        go_to(n, astate, gstate)
     } else {
-        return game::Action::Turn(ndir - astate.pos.dir)
+        roam()
     }
 }
 
 fn pony_follow(astate : &actor::State, gstate : &game::State) -> game::Action {
 
         let start = astate.pos.coord;
-
 
         let player_pos = closest_reachable(gstate, start, 10,
             |pos| gstate.actor_map_or(pos, false, |a| a.is_player())
@@ -77,7 +87,7 @@ fn pony_follow(astate : &actor::State, gstate : &game::State) -> game::Action {
         };
 
         if let Some((_, neigh)) = player_pos {
-            go_to(astate, neigh)
+            go_to(neigh, astate, gstate)
         } else {
             roam()
         }
