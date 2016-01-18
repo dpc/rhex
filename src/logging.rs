e37f04f3f8122b716bfc05cc16e7ca08ff3151c7
Dawid Ciężarkiewicz <dpc@dpc.pw>
2016-01-17 18:06:20 -0800

    Use chrono.

--- src/logging.rs
+++ src/logging.rs
@@ -1,13 +1,13 @@
 use fern;
 use log;
-use time;
+use chrono;
 
 pub fn init() {
     let logger_config = fern::DispatchConfig {
         format: Box::new(|msg: &str, level: &log::LogLevel, _location: &log::LogLocation| {
             // This is a fairly simple format, though it's possible to do more complicated ones.
             // This closure can contain any code, as long as it produces a String message.
-            format!("{} {:<6} {}", time::now().strftime("%Y-%m-%d %H:%M:%S").unwrap(), level, msg)
+            format!("{} {:<6} {}", chrono::Local::now().to_rfc3339(), level, msg)
         }),
         output: vec![fern::OutputConfig::file("debug.log")],
         level: log::LogLevelFilter::Debug,
